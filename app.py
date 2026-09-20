a7cce7d0c47c7334dd89745601992371ee4406dc
sivasai905 <sivasaidoppalapudi@gmail.com>
2026-09-20 12:09:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ DATA_PATH = "/content/ipl_deliveries.zip"
 @st.cache_data
 def load_data(path):
     df = pd.read_csv(path, compression="zip")
+      return df
 
     required_columns = [
         "match_id", "date", "venue", "innings",
